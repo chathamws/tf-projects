c67c0a268c9048bf375a3fef8c34e8a10b735439
Wayne Chatham <chathamws@yahoo.com>
2026-07-30 15:13:35 -0700

    Fix: add missing root variable declaration and repair broken syntax brackets

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ terraform {
 variable "pm_api_url" { type = string }
 variable "pm_api_token_id" { type = string }
 variable "pm_api_token_secret" { type = string }
+variable "pm_api_token_secret" { type = string }
 
 provider "proxmox" {
   endpoint  = var.pm_api_url
