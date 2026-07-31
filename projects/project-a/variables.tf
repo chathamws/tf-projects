a3e02fe0b8b84f15d1ceca5a78f0294396820321
Wayne Chatham <chathamws@yahoo.com>
2026-07-31 13:31:56 -0700

    Fix: Remove remaining semicolons from ip_address, cores, and disk_size variables

--- projects/project-a/variables.tf
+++ projects/project-a/variables.tf
@@ -9,6 +9,18 @@ variable "container_root_password" {
 
 variable "vm_id" { type = number }
 variable "hostname" { type = string }
-variable "ip_address" { type = string; default = "dhcp" }
-variable "cores" { type = number; default = 1 }
-variable "disk_size" { type = number; default = 8 }
+
+variable "ip_address" { 
+  type    = string
+  default = "dhcp" 
+}
+
+variable "cores" { 
+  type    = number
+  default = 1 
+}
+
+variable "disk_size" { 
+  type    = number
+  default = 8 
+}
