60dcc7c0dbc1e579bf975f08f802495a4b9c68b7
Wayne Chatham <chathamws@yahoo.com>
2026-08-01 16:48:14 -0700

    Cline refactor workspaces

--- projects/app-cluster/variables.tf
+++ projects/app-cluster/variables.tf
@@ -2,19 +2,3 @@ variable "container_root_password" {
   type      = string
   sensitive = true
 }
-
-variable "target_node" {
-  type = string
-}
-
-variable "hostname" {
-  type = string
-}
-
-variable "disk_size_gb" {
-  type = number
-}
-
-variable "cpu_cores" {
-  type = number
-}
--- projects/test-app/dev.tfvars
+++
@@ -1,4 +0,0 @@
-target_node  = "pve1"
-hostname     = "tf-dev-test"
-disk_size_gb = 8
-cpu_cores    = 1
