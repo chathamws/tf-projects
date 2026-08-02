bd9dd4509dc79ee95bc1a6e801fc8abdb5493e64
Wayne Chatham <chathamws@yahoo.com>
2026-08-01 17:14:39 -0700

    remove dev instanaces

--- projects/app-cluster/workspaces.tf
+++ projects/app-cluster/workspaces.tf
@@ -3,12 +3,12 @@ locals {
   # delete its block and run `terraform apply` — Terraform will automatically
   # destroy the corresponding LXC container.
   workspaces = {
-    dev = {
-      target_node  = "pve1"
-      hostname     = "tf-dev-app"
-      disk_size_gb = 8
-      cpu_cores    = 1
-    }
+    #dev = {
+    #  target_node  = "pve1"
+    #  hostname     = "tf-dev-app"
+    #  disk_size_gb = 8
+    #  cpu_cores    = 1
+    #}
     #prod = {
     #  target_node  = "pve1"
     #  hostname     = "tf-prod-app"
